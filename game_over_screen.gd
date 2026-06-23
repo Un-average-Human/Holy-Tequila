99b16ae7c2837657ea80a10456053f30f958b22f
Un-average-Human <miguelmfernandes1008@gmail.com>
2026-06-23 17:48:51 +1200

    game over menu
    works but looks ugly

--- game_over_screen.gd
+++ game_over_screen.gd
@@ -9,4 +9,20 @@ extends Control
 @export var boss_name: RichTextLabel
 
 func _ready() -> void:
-	pass
+	var boss_key = BossfightData.current_boss
+	
+	if boss_key != "" and BossfightData.boss_data.has(boss_key):
+		var data = BossfightData.boss_data[boss_key]
+		boss_name.text = boss_key
+		quote_label.text = data["winning_quote"]
+	
+	return_to_menu_button.pressed.connect(_buttons.bind(return_to_menu_button))
+	retry_button.pressed.connect(_buttons.bind(retry_button))
+
+func _buttons(button: Button):
+	match button:
+		return_to_menu_button:
+			get_tree().change_scene_to_file(BossfightData.menu)
+		retry_button:
+			get_tree().change_scene_to_file(BossfightData.current_world)
+	queue_free()
